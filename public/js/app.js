7f1cba96004b96195aa563ffc184eae5a445eeae
Gilad Reytan <gilad@football.local>
2019-08-05 15:37:02 +0300

    changing fetch to https

--- public/js/app.js
+++ public/js/app.js
@@ -62,135 +62,135 @@ gameForm.addEventListener('submit', (e) => {
 
 
 
-const ReadLiveStats = async function ()  {
-    
-    try{
-    fetch('http://football.bamboo-video.com/api/football/fastdata/' + gameID + '?format=json&tvStats=true ').then((response) => {//MATCH_INSTAT_ID
-        response.json().then((data) => {
-            
-            if (data.error) {
-                console.log('Error in JSON')
-            } else {
-                homeTeam.textContent = data.data.homeTeam.hebrewName
-                awayTeam.textContent = data.data.awayTeam.hebrewName
-            }
-            
-            //*********Goals */
-            if (data.data.gameStatsHome.GoalRegular) {
-                
-                hgoals.textContent = data.data.gameStatsHome.GoalRegular
-            } else {
-                hgoals.textContent = 0
-            }
-            
-            if (data.data.gameStatsAway.GoalRegular) {
-                agoals.textContent = data.data.gameStatsAway.GoalRegular
-            } else {
-                agoals.textContent = 0
-            }
-            
-            //*********Ball Possession */
+const ReadLiveStats = async function () {
 
-            const homeposs = Math.round((data.data.ballTimeHome) * 100 / (data.data.ballTimeHome + data.data.ballTimeAway))
-            hballPoss.textContent = homeposs + '%'
-            aballPoss.textContent = (100 - homeposs) + '%'
-            
-            //*********attempts on goal */
-            if (data.data.gameStatsHome.AttemptonGoal) {
-                hattempts.textContent = data.data.gameStatsHome.AttemptonGoal
-            } else {
-                hattempts.textContent = 0
-            }
-            
-            if (data.data.gameStatsAway.AttemptonGoal) {
-                aattempts.textContent = data.data.gameStatsAway.AttemptonGoal
-            } else {
-                aattempts.textContent = 0
-            }
-            
-            //*********attempts on target */
-            if (data.data.gameStatsHome.OnTarget) {
-                honTarget.textContent = data.data.gameStatsHome.OnTarget
-            } else {
-                honTarget.textContent = 0
-            }
-            
-            if (data.data.gameStatsAway.OnTarget) {
-                aonTarget.textContent = data.data.gameStatsAway.OnTarget
-            } else {
-                aonTarget.textContent = 0
-            }
-           
-            //*********Corners */
-            if (data.data.gameStatsHome.Corner) {
-                hcorner.textContent = data.data.gameStatsHome.Corner
-            } else {
-                hcorner.textContent = 0
-            }
-            
-            if (data.data.gameStatsAway.Corner) {
-                acorner.textContent = data.data.gameStatsAway.Corner
-            } else {
-                acorner.textContent = 0
-            }
-            
-            //*********Offsides */
-            if (data.data.gameStatsHome.Offside) {
-                hoffside.textContent = data.data.gameStatsHome.Offside
-            } else {
-                hoffside.textContent = 0
-            }
-            
-            if (data.data.gameStatsAway.Offside) {
-                aoffside.textContent = data.data.gameStatsAway.Offside
-            } else {
-                aoffside.textContent = 0
-            }
-            
-            //*********Fouls */
-            if (data.data.gameStatsHome.foul) {
-                hfoul.textContent = data.data.gameStatsHome.foul
-            } else {
-                hfoul.textContent = 0
-            }
-            
-            if (data.data.gameStatsAway.foul) {
-                afoul.textContent = data.data.gameStatsAway.foul
-            } else {
-                afoul.textContent = 0
-            }
-            
-            //*********Yellow Cards */
-            if (data.data.gameStatsHome.YellowCard) {
-                hyellow.textContent = data.data.gameStatsHome.YellowCard
-            } else {
-                hyellow.textContent = 0
-            }
+    try {
+        fetch('https://football.bamboo-video.com/api/football/fastdata/' + gameID + '?format=json&tvStats=true ').then((response) => {//MATCH_INSTAT_ID
+            response.json().then((data) => {
 
-            if (data.data.gameStatsAway.YellowCard) {
-                ayellow.textContent = data.data.gameStatsAway.YellowCard
-            } else {
-                ayellow.textContent = 0
-            }
+                if (data.error) {
+                    console.log('Error in JSON')
+                } else {
+                    homeTeam.textContent = data.data.homeTeam.hebrewName
+                    awayTeam.textContent = data.data.awayTeam.hebrewName
+                }
 
-            //*********Red Cards */
-            if (data.data.gameStatsHome.RedCard) {
-                hred.textContent = data.data.gameStatsHome.RedCard
-            } else {
-                hred.textContent = 0
-            }
-            if (data.data.gameStatsAway.RedCard) {
-                ared.textContent = data.data.gameStatsAway.RedCard
-            } else {
-                ared.textContent = 0
-            }
-            //console.log(data.data.gameStatsHome.GoalRegular,homeposs,data.data.gameStatsHome.AttemptonGoal,data.data.gameStatsHome.OnTarget,data.data.gameStatsHome.Corner,data.data.gameStatsHome.Offside,data.data.gameStatsHome.foul,data.data.gameStatsHome.YellowCard,data.data.gameStatsHome.RedCard)
+                //*********Goals */
+                if (data.data.gameStatsHome.GoalRegular) {
+
+                    hgoals.textContent = data.data.gameStatsHome.GoalRegular
+                } else {
+                    hgoals.textContent = 0
+                }
+
+                if (data.data.gameStatsAway.GoalRegular) {
+                    agoals.textContent = data.data.gameStatsAway.GoalRegular
+                } else {
+                    agoals.textContent = 0
+                }
+
+                //*********Ball Possession */
+
+                const homeposs = Math.round((data.data.ballTimeHome) * 100 / (data.data.ballTimeHome + data.data.ballTimeAway))
+                hballPoss.textContent = homeposs + '%'
+                aballPoss.textContent = (100 - homeposs) + '%'
+
+                //*********attempts on goal */
+                if (data.data.gameStatsHome.AttemptonGoal) {
+                    hattempts.textContent = data.data.gameStatsHome.AttemptonGoal
+                } else {
+                    hattempts.textContent = 0
+                }
+
+                if (data.data.gameStatsAway.AttemptonGoal) {
+                    aattempts.textContent = data.data.gameStatsAway.AttemptonGoal
+                } else {
+                    aattempts.textContent = 0
+                }
+
+                //*********attempts on target */
+                if (data.data.gameStatsHome.OnTarget) {
+                    honTarget.textContent = data.data.gameStatsHome.OnTarget
+                } else {
+                    honTarget.textContent = 0
+                }
+
+                if (data.data.gameStatsAway.OnTarget) {
+                    aonTarget.textContent = data.data.gameStatsAway.OnTarget
+                } else {
+                    aonTarget.textContent = 0
+                }
+
+                //*********Corners */
+                if (data.data.gameStatsHome.Corner) {
+                    hcorner.textContent = data.data.gameStatsHome.Corner
+                } else {
+                    hcorner.textContent = 0
+                }
+
+                if (data.data.gameStatsAway.Corner) {
+                    acorner.textContent = data.data.gameStatsAway.Corner
+                } else {
+                    acorner.textContent = 0
+                }
+
+                //*********Offsides */
+                if (data.data.gameStatsHome.Offside) {
+                    hoffside.textContent = data.data.gameStatsHome.Offside
+                } else {
+                    hoffside.textContent = 0
+                }
+
+                if (data.data.gameStatsAway.Offside) {
+                    aoffside.textContent = data.data.gameStatsAway.Offside
+                } else {
+                    aoffside.textContent = 0
+                }
+
+                //*********Fouls */
+                if (data.data.gameStatsHome.foul) {
+                    hfoul.textContent = data.data.gameStatsHome.foul
+                } else {
+                    hfoul.textContent = 0
+                }
+
+                if (data.data.gameStatsAway.foul) {
+                    afoul.textContent = data.data.gameStatsAway.foul
+                } else {
+                    afoul.textContent = 0
+                }
+
+                //*********Yellow Cards */
+                if (data.data.gameStatsHome.YellowCard) {
+                    hyellow.textContent = data.data.gameStatsHome.YellowCard
+                } else {
+                    hyellow.textContent = 0
+                }
+
+                if (data.data.gameStatsAway.YellowCard) {
+                    ayellow.textContent = data.data.gameStatsAway.YellowCard
+                } else {
+                    ayellow.textContent = 0
+                }
+
+                //*********Red Cards */
+                if (data.data.gameStatsHome.RedCard) {
+                    hred.textContent = data.data.gameStatsHome.RedCard
+                } else {
+                    hred.textContent = 0
+                }
+                if (data.data.gameStatsAway.RedCard) {
+                    ared.textContent = data.data.gameStatsAway.RedCard
+                } else {
+                    ared.textContent = 0
+                }
+                //console.log(data.data.gameStatsHome.GoalRegular,homeposs,data.data.gameStatsHome.AttemptonGoal,data.data.gameStatsHome.OnTarget,data.data.gameStatsHome.Corner,data.data.gameStatsHome.Offside,data.data.gameStatsHome.foul,data.data.gameStatsHome.YellowCard,data.data.gameStatsHome.RedCard)
+            })
+
+
         })
-        
-
-    })
-}catch(e){
-    console.log(e.message)
-}
+    } catch (e) {
+        console.log(e.message)
+    }
 
 }
